0cff842ec1f87fadcf0bab63ce0bd67e9d8d9f03
HanafiHaruka <muhammadhanafi090407@gmail.com>
2025-11-17 19:10:57 +0700

    update

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -1,5 +1,5 @@
 export const portfolioData = {
-  name: "Mhd Hanafi Ginting",
+  name: "Muhammad Hanafi Ginting",
   role: "Mahasiswa Teknik Informatika",
   university: "Universitas Budi Dharma",
   location: "Medan, Indonesia",
